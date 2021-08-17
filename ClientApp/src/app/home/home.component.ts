cd862db54e64cfa0732053f81cfb7dc99e34772f
Vitoria Mendes <mendes.tecnologia.dev@gmail.com>
2021-08-17 18:25:49 -0300

    Improving homepage

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthorizeService } from '../../api-authorization/authorize.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public userName: Observable<string>;
+
+  constructor(private authorizeService: AuthorizeService) { }
+
+  ngOnInit() {
+    this.userName = this.authorizeService.getUser().pipe(map(u => u && u.name));
+  }
 }
