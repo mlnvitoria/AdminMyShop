fec39cba7da5d6bfb278b34fecfd8ac7cf491a62
Vitoria Mendes <mendes.tecnologia.dev@gmail.com>
2021-08-18 21:13:28 -0300

    New product list component

--- ClientApp/src/app/products/list/list.component.ts
+++ ClientApp/src/app/products/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { Observable } from 'rxjs';
+import { ProductDetailsComponent } from '../details/details.component';
 
 import { ProductService, Product } from '../product.service';
 
@@ -8,17 +10,26 @@ import { ProductService, Product } from '../product.service';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ProductListComponent implements OnInit {
+  displayedColumns = ['id', 'name', 'price', 'quantity', 'actions'];
+
   protected products: Product[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, public dialog: MatDialog) { }
 
   getProducts(): void {
     this.productService.getProducts().subscribe(products => this.products = products);
   }
 
+  openDetails(product: Product): void {
+    this.dialog.open(ProductDetailsComponent, {
+      width: '40em',
+      data: product
+    })
+  }
+
   delete(product: Product): void {
-    this.productService.deleteProduct(product.Id).subscribe();
+    this.productService.deleteProduct(product.id).subscribe();
   }
 
   ngOnInit() {
